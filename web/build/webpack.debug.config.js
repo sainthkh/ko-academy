aeaad0d111c0f36213e1a76824c2cfa022284cf9
sainthkh <sainthkh@naver.com>
2016-12-02 11:55:53 +0900

    Added react-css-modules configuration.

--- web/build/webpack.debug.config.js
+++ web/build/webpack.debug.config.js
@@ -27,6 +27,12 @@ module.exports = {
 				cacheDirectory: './temp/babel_cache',
 				presets: ['react', 'es2015']
 			}
+		}, {
+			test: /\.css$/,
+			loaders: [
+				'style?sourceMap',
+				'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
+			]
 		}]
 	},
 	plugins:[] 
