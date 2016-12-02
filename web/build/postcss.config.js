b6efea7ff0ef0d76b5100ad4b11560ec91c45ef8
sainthkh <sainthkh@naver.com>
2016-12-02 17:23:33 +0900

    Set up postcss.

---
+++ web/build/postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	plugins: [
+		require('postcss-utilities'),
+		require('precss'),
+		require('postcss-short'),
+		require('postcss-cssnext'),
+	]
+}
--- web/build/webpack.debug.config.js
+++ web/build/webpack.debug.config.js
@@ -31,7 +31,8 @@ module.exports = {
 			test: /\.css$/,
 			loaders: [
 				'style?sourceMap',
-				'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
+				'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+				'postcss?config=./build/postcss.config.js',
 			]
 		}]
 	},
