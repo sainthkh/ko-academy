0b513c42b0978e64d01fbaf66cac529e1edbf635
sainthkh <sainthkh@naver.com>
2016-12-03 19:46:54 +0900

    Changed AppRoutes to App.

--- web/app/client.js
+++ web/app/client.js
@@ -2,8 +2,8 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRoutes from './components/AppRoutes';
+import App from './components/App';
 
 window.onload = () => {
-	ReactDOM.render(<AppRoutes />, document.getElementById('main'));
+	ReactDOM.render(<App />, document.getElementById('main'));
 };
--- web/app/components/App.js
+++ web/app/components/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 import { Router, browserHistory } from 'react-router';
 import routes from '../routes';
 
-export default class AppRoutes extends React.Component {
+export default class App extends React.Component {
   render() {
     return (
       <Router history={browserHistory} routes={routes} onUpdate={() => window.scrollTo(0, 0)}/>
