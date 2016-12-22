1763d503a6bac8f85fd6a6955c35c60c1e628fd3
sainthkh <sainthkh@naver.com>
2016-12-22 10:34:03 +0900

    Fixed the reducer to meet the new state.

--- web/app/auth/signup/reducer.ts
+++ web/app/auth/signup/reducer.ts
@@ -9,36 +9,39 @@ import {
 	SERVER_DOWN, PAGE_NOT_FOUND, INTERNAL_SERVER_ERROR, OTHER_ERROR,
 } from '../../data/fetch'
 
+import {
+	DIALOG_WAITING, DIALOG_DONE, DIALOG_ERROR,
+} from '../../common/dialog'
+
 const initialState = Map<string, any>({
 	username: "guest",
 	token: null,
-	waitingSignUp: false,
-	signupDialog: Map({
-		on: true,
-		error: null,
-	}), 
-	error: null,
+	accessLevel: "guest",
+	dialog: false,
 })
 
 export function signup(state=initialState, action) {
 	switch(action.type) {
 		case REQUEST_SIGNUP:
-			return state.set('waitingSignUp', true)
+			return state.set('dialog', Map({
+				status: DIALOG_WAITING,
+				error: null,
+			}))
 		case SUCCEEDED_SIGNUP:
 			return state.withMutations(state => {
 				state.set('username', action.username)
 					.set('token', action.token)
-					.set('waitingSignUp', false)
-					.set('signupDialog', false)
-			})
-		case FAILED_SIGNUP:
-			return state.withMutations(state => {
-				state.set('waitingSignUp', false)
-					.set('signupDialog', Map({
-						on: true, 
-						error: List(action.error)
+					.set('accessLevel', action.accessLevel)
+					.set('dialog', Map({
+						status: DIALOG_DONE,
+						error: null
 					}))
 			})
+		case FAILED_SIGNUP:
+			return state.set('dialog', Map({
+				status: DIALOG_ERROR, 
+				error: List(action.error)
+			}))
 	}
 	return state
 }
--- web/app/auth/signup/test/reducer.ts
+++ web/app/auth/signup/test/reducer.ts
@@ -17,18 +17,18 @@ import {
 	SERVER_DOWN, PAGE_NOT_FOUND, INTERNAL_SERVER_ERROR, OTHER_ERROR,
 } from '../../../data/fetch'
 
+import {
+	DIALOG_WAITING, DIALOG_DONE, DIALOG_ERROR,
+} from '../../../common/dialog'
+
 test("signup with initialState and the empty action", t => {
 	var resultState = signup(undefined, {})
 
 	t.immutableEqual(resultState, Map({
 		username: "guest",
 		token: null,
-		waitingSignUp: false,
-		signupDialog: Map({
-			on: true,
-			error: null,
-		}), 
-		error: null,
+		accessLevel: "guest",
+		dialog: false,
 	}), "current state is returned")
 	t.end()
 })
@@ -41,13 +41,12 @@ test("signup with initialState and requestSignup", t => {
 	t.immutableEqual(resultState, Map({
 		username: "guest",
 		token: null,
-		waitingSignUp: true,
-		signupDialog: Map({
-			on: true,
+		accessLevel: "guest",
+		dialog: Map({
+			status: DIALOG_WAITING,
 			error: null,
 		}), 
-		error: null,
-	}), "waitingSignUp became true")
+	}), "dialog object is created and status is added")
 
 	t.end()
 })
@@ -57,14 +56,17 @@ test("signup with initialState and receivedSignup:success", t => {
 		type: SUCCEEDED_SIGNUP,
 		username: "signedup-username",
 		token: "random-string",
+		accessLevel: "free",
 	})
 
 	t.immutableEqual(resultState, Map({
 		username: "signedup-username",
 		token: "random-string",
-		waitingSignUp: false,
-		signupDialog: false,
-		error: null,
+		accessLevel: "free",
+		dialog: Map({
+			status: DIALOG_DONE,
+			error: null
+		}),
 	}), "username, token should be indentical with action. waitingSignUp and signupDialog should be false")
 
 	t.end()
@@ -83,35 +85,32 @@ test("signup with initialState and receivedSignup:failed", t => {
 	t.immutableEqual(resultState, Map({
 		username: "guest",
 		token: null,
-		waitingSignUp: false,
-		signupDialog: Map({
-			on: true,
+		accessLevel: "guest",
+		dialog: Map({
+			status: DIALOG_ERROR,
 			error: List([
 				LONG_USERNAME,
 				DUPLICATE_EMAIL,
 				SHORT_PASSWORD,
 			])
 		}), 
-		error: null,
 	}), "signupDialog contains errors correctly")
 	t.end()
 })
-
 
 test("signup with failedSignup and requestSignup", t => {
 	var state = Map({
 		username: "guest",
 		token: null,
-		waitingSignUp: false,
-		signupDialog: Map({
-			on: true,
+		accessLevel: "guest",
+		dialog: Map({
+			status: DIALOG_ERROR,
 			error: List([
 				LONG_USERNAME,
 				DUPLICATE_EMAIL,
 				COMMON_PASSWORD,
 			]),
 		}), 
-		error: null,
 	})
 	var resultState = signup(state, {
 		type: REQUEST_SIGNUP,
@@ -120,16 +119,11 @@ test("signup with failedSignup and requestSignup", t => {
 	t.immutableEqual(resultState, Map({
 		username: "guest",
 		token: null,
-		waitingSignUp: true,
-		signupDialog: Map({
-			on: true,
-			error: List([
-				LONG_USERNAME,
-				DUPLICATE_EMAIL,
-				COMMON_PASSWORD,
-			]),
+		accessLevel: "guest",
+		dialog: Map({
+			status: DIALOG_WAITING,
+			error: null,
 		}), 
-		error: null,
 	}), "waitingSignUp became true")
 
 	t.end()
@@ -139,28 +133,30 @@ test("signup with initialState and receivedSignup:success", t => {
 	var state = Map({
 		username: "guest",
 		token: null,
-		waitingSignUp: false,
-		signupDialog: Map({
-			on: true,
+		accessLevel: "guest",
+		dialog: Map({
+			status: DIALOG_ERROR,
 			error: List([
 				LONG_USERNAME,
 				COMMON_PASSWORD,
 			]),
 		}), 
-		error: null,
 	})
 	var resultState = signup(state, {
 		type: SUCCEEDED_SIGNUP,
 		username: "finallypassed",
 		token: "random-string-2",
+		accessLevel: "gold",
 	})
 
 	t.immutableEqual(resultState, Map({
 		username: "finallypassed",
 		token: "random-string-2",
-		waitingSignUp: false,
-		signupDialog: false,
-		error: null,
+		accessLevel: "gold",
+		dialog: Map({
+			status: DIALOG_DONE,
+			error: null,
+		})
 	}), "username, token should be indentical with action. waitingSignUp and signupDialog should be false")
 
 	t.end()
@@ -170,15 +166,14 @@ test("signup with initialState and receivedSignup:failed", t => {
 	var state = Map({
 		username: "guest",
 		token: null,
-		waitingSignUp: false,
-		signupDialog: Map({
-			on: true,
+		accessLevel: "guest",
+		dialog: Map({
+			status: DIALOG_ERROR,
 			error: List([
 				DUPLICATE_EMAIL,
 				COMMON_PASSWORD,
 			]),
 		}), 
-		error: null,
 	})
 	var resultState = signup(undefined, {
 		type: FAILED_SIGNUP,
@@ -190,14 +185,13 @@ test("signup with initialState and receivedSignup:failed", t => {
 	t.immutableEqual(resultState, Map({
 		username: "guest",
 		token: null,
-		waitingSignUp: false,
-		signupDialog: Map({
-			on: true,
+		accessLevel: "guest",
+		dialog: Map({
+			status: DIALOG_ERROR,
 			error: List([
 				SHORT_PASSWORD,
 			])
 		}), 
-		error: null,
 	}), "signupDialog contains new errors correctly")
 	t.end()
 })
