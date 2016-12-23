e89e18d078e66d08de01033ac775725666aced67
sainthkh <sainthkh@naver.com>
2016-12-23 17:50:42 +0900

    Added final done page in SignupDialog.

--- web/app/auth/signup/SignupDialogContent.tsx
+++ web/app/auth/signup/SignupDialogContent.tsx
@@ -17,51 +17,47 @@ class SignupDialogContentReact extends React.Component<SignupDialogContentProps,
 	constructor(props) {
 		super(props)
 		this.submit = this.submit.bind(this)
+		this.form = this.form.bind(this)
+		this.loading = this.loading.bind(this)
+		this.done = this.done.bind(this)
 		this.ID = "signup"
 	}
 
 	render() {
 		if(!this.props.waiting) {
-			var title = "Sign up and start your Korean journey!"
-			var main = (
-				<div>
-					<form method="POST" name="signup" role="form" onSubmit={this.submit}>
-						<input type="text" styleName="field" name="username" id="username" placeholder="user name" />
-						{this.props.error.longName && (<div styleName="invalid">Your name is too long. User name should be shorter than 50 characters.</div>)}
-						<input type="email" styleName="field" name="email" id="email" placeholder="email" />
-						{this.props.error.invalidEmail && (<div styleName="invalid">Your email format is invalid. Please check it out.</div>)}
-						{this.props.error.duplicateEmail && (<div styleName="invalid">You have already signed up.</div>)}
-						<input type="password" styleName="field" name="password" id="password" placeholder="password" />
-						{this.props.error.shortPassword && (<div styleName="invalid">Your password is too short. Password should be at least 8 characters.</div>)}
-						{this.props.error.commonPassword && (<div styleName="invalid">Your password is too common. Please use another password.</div>)}
-						<button type="submit" styleName="submit">Sign up</button>
-					</form>
-				</div>
-			)
-			var footer = (
-				<div>
-					Did you mean to <a href="#login" onClick={ e => openDialog(e, 'login')}>login</a>?
-				</div>
-			)
-			return <Dialog ID={this.ID} title={title} main={main} footer={footer}/>;
+			if(!this.props.done) {
+				return this.form()
+			} else {
+				return this.done()
+			}
 		} else {
-			var main = (
-				<div styleName="message">
-					<Spinner />
-					<div>
-						<p>now signing up...</p>
-						<p>please wait.</p>
-					</div>
-				</div>
-			)
-			return <Dialog ID={this.ID} freeze={true} main={main} />
+			return this.loading()
 		}
 	}
 
-	componentDidUpdate() {
-		if(this.props.done) {
-			closeDialog(this.ID)
-		}
+	private form() {
+		var title = "Sign up and wait for your Korean journey!"
+		var main = (
+			<div>
+				<form method="POST" name="signup" role="form" onSubmit={this.submit}>
+					<input type="text" styleName="field" name="username" id="username" placeholder="user name" />
+					{this.props.error.longName && (<div styleName="invalid">Your name is too long. User name should be shorter than 50 characters.</div>)}
+					<input type="email" styleName="field" name="email" id="email" placeholder="email" />
+					{this.props.error.invalidEmail && (<div styleName="invalid">Your email format is invalid. Please check it out.</div>)}
+					{this.props.error.duplicateEmail && (<div styleName="invalid">You have already signed up.</div>)}
+					<input type="password" styleName="field" name="password" id="password" placeholder="password" />
+					{this.props.error.shortPassword && (<div styleName="invalid">Your password is too short. Password should be at least 8 characters.</div>)}
+					{this.props.error.commonPassword && (<div styleName="invalid">Your password is too common. Please use another password.</div>)}
+					<button type="submit" styleName="submit">Sign up</button>
+				</form>
+			</div>
+		)
+		var footer = (
+			<div>
+				Did you mean to <a href="#login" onClick={ e => openDialog(e, 'login')}>login</a>?
+			</div>
+		)
+		return <Dialog ID={this.ID} title={title} main={main} footer={footer}/>;
 	}
 
 	private submit(e) {
@@ -75,6 +71,30 @@ class SignupDialogContentReact extends React.Component<SignupDialogContentProps,
 		}
 
 		this.props.fetchSignup(user)
+	}
+
+	private loading() {
+		var main = (
+			<div styleName="message">
+				<Spinner />
+				<div>
+					<p>now signing up...</p>
+					<p>please wait.</p>
+				</div>
+			</div>
+		)
+		return <Dialog ID={this.ID} freeze={true} main={main} />
+	}
+
+	private done() {
+		var title = "Thank you for signing up."
+		var main = (
+			<div styleName="message">
+				<p>I'll contact you soon when I open the site. </p>
+				<p><a href="/uploads/hangeul-practice">Here is your gift.</a></p>
+			</div>
+		)
+		return <Dialog ID={this.ID} title={title} main={main} />
 	}
 }
 
