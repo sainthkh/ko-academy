70215e9d2e9eac4d1c004595dd808bbf6de91a7e
sainthkh <sainthkh@naver.com>
2016-12-23 17:50:16 +0900

    Changed the port number of content server.

--- content/index.js
+++ content/index.js
@@ -1,5 +1,5 @@
 const server = require('./src').default
-const port = process.env.PORT ? process.env.PORT: 3001
+const port = process.env.PORT ? process.env.PORT: 4000
 
 server.listen(port, "localhost", function() {
   console.log(server.name +' listening at '+ server.url);
