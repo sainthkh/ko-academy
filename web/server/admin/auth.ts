b171b82bc3a1aea8dbe00850aad32d5f9007bd42
sainthkh <sainthkh@naver.com>
2017-01-02 16:46:33 +0900

    Added login admin api resource skeleton.

---
+++ web/server/admin/auth.ts
@@ -0,0 +1,9 @@
+import * as express from 'express'
+
+const router = express.Router()
+
+router.post('/login', (req, res) => {
+	
+})
+
+export default router
--- web/server/admin/index.ts
+++ web/server/admin/index.ts
@@ -1,8 +1,11 @@
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
 
+import { default as auth } from './auth'
+
 var router = express.Router()
 
 router.use(bodyParser.json())
+router.use('/auth', auth)
 
 export default router
