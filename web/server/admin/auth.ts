a58ff521b74fd243e5bc1fbada133ae5a4be1324
sainthkh <sainthkh@naver.com>
2017-01-03 16:23:42 +0900

    Added login REST resource.

--- web/server/admin/auth.ts
+++ web/server/admin/auth.ts
@@ -1,9 +1,30 @@
 import * as express from 'express'
+import * as jwt from 'jsonwebtoken'
+var sodium = require('sodium').api
+
+import * as fs from 'fs'
+import * as path from 'path'
 
 const router = express.Router()
 
 router.post('/login', (req, res) => {
-	
+	var config = JSON.parse(fs.readFileSync(path.join(__dirname, './config.json')).toString())
+	config.password = Buffer.from(config.password, 'base64')
+
+	if (req.body.id == config.id && 
+		sodium.crypto_pwhash_str_verify(config.password, Buffer.from(req.body.password, 'utf8'))) {
+		var token = jwt.sign({ id: config.id}, config.secret, {
+			expiresIn: "30m" // expires in 24 hours
+        });
+		res.json({
+			success: true,
+			token: token,
+		})
+	} else {
+		res.json({
+			success: false,
+		})
+	}
 })
 
 export default router
--- web/server/admin/index.ts
+++ web/server/admin/index.ts
@@ -1,5 +1,9 @@
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
+import * as jwt from 'jsonwebtoken'
+
+import * as fs from 'fs'
+import * as path from 'path'
 
 import { default as auth } from './auth'
 
@@ -7,5 +11,25 @@ var router = express.Router()
 
 router.use(bodyParser.json())
 router.use('/auth', auth)
+router.use((req, res, next) => {
+	if(req.body.token) {
+		var config = JSON.parse(fs.readFileSync(path.join(__dirname, './config.json')).toString())
+
+		jwt.verify(req.body.token, config.secret, (err, decoded) => {  
+			if (err) {
+				return res.json({
+					access: false,
+				})
+			} else {
+				(<any>req).user = decoded;
+				next()
+			}
+		})
+	} else {
+		res.json({
+			access: false,
+		})
+	}
+})
 
 export default router
