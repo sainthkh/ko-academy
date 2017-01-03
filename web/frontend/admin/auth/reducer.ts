96d6176790846291419fa64c921284cc2469277f
sainthkh <sainthkh@naver.com>
2017-01-03 20:04:09 +0900

    Added reducer for admin/auth.

--- web/frontend/admin/auth/reducer.ts
+++ web/frontend/admin/auth/reducer.ts
@@ -1,3 +1,26 @@
-export function auth(state = {}, action) {
+import {
+	REQUEST_FETCH, SUCCEEDED_FETCH, FAILED_FETCH
+} from '../../common/lib/fetch-action'
+
+import {
+	immutable
+} from '../../common/lib/immutable'
+
+export function auth(state = { stage: null, token: null }, action) {
+	switch(action.type.stage) {
+		case REQUEST_FETCH:
+			return immutable(state, {
+				stage: REQUEST_FETCH,
+			})
+		case SUCCEEDED_FETCH:
+			return immutable(state, {
+				stage: SUCCEEDED_FETCH,
+				token: action.token,
+			})
+		case FAILED_FETCH:
+			return immutable(state, {
+				stage: FAILED_FETCH
+			})
+	}
 	return state
 }
