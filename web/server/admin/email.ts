cbe9f8eaa363cdc00622464a95917702a7dcb4b2
sainthkh <sainthkh@naver.com>
2017-01-04 12:10:11 +0900

    Applied sendmail to /broadcast.

--- web/server/admin/email.ts
+++ web/server/admin/email.ts
@@ -1,17 +1,20 @@
 import * as express from 'express'
 import { Broadcast, BroadcastRecord } from '../common/data'
+import { sendmail } from '../common/email'
 
 const router = express.Router()
 
 router.post('/broadcast', (req, res) => {
+	const { list, title, content } = req.body
 	Broadcast.then(db => {
 		return db.create({
-			to: req.body.list,
+			to: list,
 			time: new Date(),
-			title: req.body.title,
-			content: req.body.content,
+			title: title,
+			content: content,
 		})
 		.then((result: BroadcastRecord) => {
+			sendmail(list, title, content)
 			res.json({
 				success: true,
 				ID: result.ID,
