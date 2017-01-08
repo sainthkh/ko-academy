e85d4d1aae91d45e9bf66a34530a3bf6ab8f90e4
sainthkh <sainthkh@naver.com>
2017-01-08 16:31:18 +0900

    Changed admin index page to use fetch3.

--- web/frontend/admin/pages/index/Page.tsx
+++ web/frontend/admin/pages/index/Page.tsx
@@ -1,26 +1,22 @@
 import { connect } from 'react-redux'
 
-import { fetchAction, fetchProps } from '../../../common/lib/fetch'
+import { fetchProps2, submit } from '../../../common/lib/fetch'
 import { setToken } from '../../../common/lib/token'
 import { Layout } from './Layout'
 
-const mapStateToProps = (state) => {
-	return fetchProps(state.auth)
-}
+const mapStateToProps = state => fetchProps2(state.fetch)
 
 const mapDispatchToProps = dispatch => {
 	return {
 		submit: user => {
-			dispatch(fetchAction({
+			dispatch(submit({
 				admin: true,
-				name: "login",
+				id: "login",
 				resource: "/auth/login",
 				processResult: result => {
-					let action = {} as any
 					if (result.success) {
 						setToken(result.token)
 					} 
-					return action
 				}
 			})(user))
 		}
