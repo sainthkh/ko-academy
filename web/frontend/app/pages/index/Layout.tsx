192867fd32d7d85c000decb9fcd7a56ef212631f
sainthkh <sainthkh@naver.com>
2017-01-09 16:08:38 +0900

    Removed CSS Module.

--- web/frontend/app/pages/index/Layout.tsx
+++ web/frontend/app/pages/index/Layout.tsx
@@ -3,17 +3,20 @@
 import * as React from 'react';
 import MainBar from '../common/menu/MainBar'
 import Hero from './Hero'
-import * as CSSModules from 'react-css-modules'
-import styles from "./Layout.css"
 
 class IndexPage extends React.Component<{}, {}> {
 	render() {
 		return (
-			<div styleName="wrap">
+			<div style={wrap}>
 				<Hero />
 			</div>
 		);
 	}
 }
 
-export default CSSModules(IndexPage, styles)
+var wrap = {
+	background: '#192867',
+	height: '100%',
+}
+
+export default IndexPage
