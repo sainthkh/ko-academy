a62fa00edca4733ca1c4ff514d08e39a3d04adb6
sainthkh <sainthkh@naver.com>
2017-01-10 11:40:01 +0900

    Connected LoginDialog with server.

--- web/frontend/app/pages/common/auth/LoginDialog.tsx
+++ web/frontend/app/pages/common/auth/LoginDialog.tsx
@@ -4,17 +4,23 @@ import { default as Dialog, openDialog, closeDialog } from '../Dialog'
 import { default as Spinner } from '../Spinner'
 import { FetchProps } from '../../../../common/lib/fetch/props'
 import { FetchableComponent } from '../../../../common/lib/fetch'
+import { DialogLayout } from './AuthDialog'
 
 export interface LayoutProps extends FetchProps {
 }
 
 
-class Layout extends React.Component<LayoutProps, {}> {
-	render() {
+class Layout extends DialogLayout<LayoutProps, {}> {
+	constructor(props) {
+		super(props)
+		this.formName = "login"
+	}
+
+	protected main() {
 		var title = "Resume your Korean journey!"
 		var main = (
 			<div>
-				<form method="POST" role="form">
+				<form method="POST" name={this.formName} role="form" onSubmit={this.submit}>
 					<input type="email" styleName="field" name="email" id="email" placeholder="email" />
 					<input type="password" styleName="field" name="password" id="password" placeholder="password" />
 					<button type="submit" styleName="submit">Login</button>
@@ -28,9 +34,31 @@ class Layout extends React.Component<LayoutProps, {}> {
 		)
 		return <Dialog ID="login" title={title} main={main} footer={footer}/>;
 	}
+
+	protected loading() {
+		var main = (
+			<div styleName="message">
+				<Spinner />
+				<div>
+					<p>now logging in...</p>
+					<p>please wait.</p>
+				</div>
+			</div>
+		)
+		return <Dialog ID={this.formName} freeze={true} main={main} />
+	}
+
+	protected done() {
+		var main = (
+			<div styleName="message">
+				<p>Welcome back!</p>
+			</div>
+		)
+		return <Dialog ID={this.formName} main={main} />
+	}
 }
 
 export const LoginDialog = FetchableComponent({
 	id: "login",
-	resouce: "/login"
+	resource: "/login"
 })(Layout)
