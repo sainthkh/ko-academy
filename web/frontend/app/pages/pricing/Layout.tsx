316a1f60a49294a0474193f1ceed73b7cb14c0c4
sainthkh <sainthkh@naver.com>
2017-01-17 09:45:29 +0900

    Added pricing page html&css.

--- web/frontend/app/pages/pricing/Layout.tsx
+++ web/frontend/app/pages/pricing/Layout.tsx
@@ -1,14 +1,69 @@
 'use strict';
 
 import * as React from 'react';
+import * as CSSModules from 'react-css-modules'
+import MainBar from '../common/menu/MainBar'
+import { openDialog } from '../common/Dialog'
+import { PaymentDialog } from './PaymentDialog'
 
-class PricingPage extends React.Component<{}, {}> {
+import styles from './style.css'
+
+class Layout extends React.Component<{}, {}> {
 	render() {
 		return (
 			<div>
-				Pricing
+				<MainBar />
+				<div styleName="hero">
+					<div className="wrap">
+						<h1>Learn Korean in Faster, Better Way</h1>
+						<h2>$9.99/month</h2>
+						<a href="#" onClick={e => openDialog(e, "payment")}>Join Now</a>
+					</div>
+				</div>
+				<div className="wrap">
+					<div styleName="benefits">
+						<h1>What's included?</h1>
+						<div styleName="benefit">
+							<h2>Learn faster with quizzes.</h2>
+							<p>Your brain loves quizzes. Scientifically, you can learn something faster by checking what you have learned. Get a boost to your Korean language by joining Gold Plan.</p>
+						</div>
+						<div styleName="benefit">
+							<h2>Is that all?</h2>
+							<p>WiseInit Academy is just born. It’s just the beginning. Please wait and see how it grows!</p>
+						</div>
+					</div>
+					<div styleName="faq">
+						<h1>FAQ</h1>
+						<div styleName="question">
+							<h2>I know nothing about Korean. Is that OK?</h2>
+							<p>Actually, you've found the right place. WiseInit Academy is the place for everyone who wants to start learning Korean. You'll learn from basic characters and how to pronounce them.</p>
+							<p>And then, you'll learn a lot about grammars and others!</p>
+						</div>
+						<div styleName="question">
+							<h2>What is your refund policy?</h2>
+							<p>We don't want your money if you're not amazingly happy with our product. Feel free to contact us at academy@mg.wiseinit.com. We'll refund without any excuses.</p>
+						</div>
+						<div styleName="question">
+							<h2>What is the payment method?</h2>
+							<p>Only credit card is accepted now. We'll add more methods later.</p>
+						</div>
+						<div styleName="question">
+							<h2>Is it safe?</h2>
+							<p>We don't save your credit card information in our server. That information is safely handled by <a href="http://stripe.com">Stripe</a>.</p>
+						</div>
+						<div styleName="question">
+							<h2>Can I upgrade later?</h2>
+							<p>Certainly! Whenever you want to learn Korean fast, you can sign up.</p>
+						</div>
+						<div styleName="question">
+							<h2>How can I cancel my subscription?</h2>
+							<p>Currently, you can cancel your subscription by sending us an email(academy@mg.wiseinit.com).</p>
+						</div>
+					</div>
+				</div>
+				<PaymentDialog />
 			</div>
 		);
 	}
 }
-export default PricingPage
+export default CSSModules(Layout, styles)
---
+++ web/frontend/app/pages/pricing/PaymentDialog.tsx
@@ -0,0 +1,86 @@
+import * as React from 'react'
+import { FetchProps } from '../../../common/lib/fetch/props'
+import { FetchableComponent } from '../../../common/lib/fetch'
+
+import Spinner from '../common/Spinner'
+import Dialog from '../common/Dialog'
+import { DialogLayout } from '../common/FetchableDialog'
+
+declare var Stripe: any
+
+class Layout extends DialogLayout<FetchProps, {}> {
+	main() {
+		let title = "Join Now and Learn More About Korean Faster!"
+		let main = (
+			<div>
+				<form name="payment-form" id="payment-form" onSubmit={this.submit}>
+					<span className="payment-errors"></span>
+					<input type="text" styleName="field" size={20} data-stripe="number" placeholder="card number"/>
+					<div styleName="card-info">
+						<div styleName="expiration">
+							<label>Expiration</label>
+							<input type="text" style={{width:"25%"}} styleName="field" size={2} data-stripe="exp_month" placeholder="MM"/>
+							<input type="text" style={{width:"25%"}} styleName="field" size={2} data-stripe="exp_year" placeholder="YY"/>
+						</div>
+						<div styleName="cvc">
+							<label>CVC</label>
+							<input type="text" style={{width:"50%"}} styleName="field" size={4} data-stripe="cvc" placeholder="cvc"/>
+						</div>
+					</div>
+					<button type="submit" id="submit-payment" styleName="submit">Pay</button>
+				</form>
+				<script type="text/javascript" src="https://js.stripe.com/v2/"></script>
+			</div>
+		)
+		return <Dialog ID="payment" title={title} main={main} />
+	}
+
+	protected loading() {
+		var main = (
+			<div styleName="message">
+				<Spinner />
+				<div>
+					<p>now signing up...</p>
+					<p>please wait.</p>
+				</div>
+			</div>
+		)
+		return <Dialog ID={this.formName} freeze={true} main={main} />
+	}
+
+	protected done() {
+		var title = "Thank you for signing up."
+		var main = (
+			<div styleName="message">
+				<p>I'll contact you soon when I open the site. </p>
+				<p><a href="/uploads/hangeul-practice">Here is your gift.</a></p>
+			</div>
+		)
+		return <Dialog ID={this.formName} title={title} main={main} />
+	}
+
+	protected submit(e) {
+		e.preventDefault()
+
+		let button = document.getElementById('submit-payment') as HTMLInputElement
+		button.disabled = true
+
+		Stripe.setPublishableKey('pk_test_EqP0OlXN1LSgmF42nzhh3TKs');
+		Stripe.card.createToken(document.forms['payment-form'], (err, res) => {
+			this.props.submit({
+				token: res.id
+			})
+		});
+	}
+
+	constructor(props) {
+		super(props)
+
+		this.submit = this.submit.bind(this)
+	}
+}
+
+export const PaymentDialog = FetchableComponent({
+	id: "payment",
+	resource: "/payment/subscription/gold",
+})(Layout)
